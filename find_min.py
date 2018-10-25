9c7742eef0d0442f8dd81448cb315283583fe60e
Batyrbek-Kuandyk <43665407+Batyrbek-Kuandyk@users.noreply.github.com>
2018-10-25 23:48:41 +0600

    Update find_min.py

--- find_min.py
+++ find_min.py
@@ -2,49 +2,67 @@
 
 def get_min(a, b):
     """
-        return min number among a and b
+        Return min number among a and b.
     """
-
+    return a if a<b else b;
 
 def get_min_without_arguments():
     """
-        raise TypeError exception with message
+        Raise TypeError exception with message.
     """
-
+    raise TypeError('Exception');
 
 def get_min_with_one_argument(x):
     """
-        return that value
+        Return that value.
     """
+    return x;
 
 
 def get_min_with_many_arguments(*args):
     """
-        return smallest number among args
+        Return smallest number among args.
     """
+    result = float('inf')
+    for arg in args:
+        if arg < result:
+            result = arg
+    return result
 
 
 def get_min_with_one_or_more_arguments(first, *args):
     """
-        return smallest number among first + args
+        Return smallest number among first + args.
     """
-
+    result = float('inf')
+    for arg in (first,) + args:
+        if arg < result:
+            result = arg
+    return result
 
 def get_min_bounded(*args, low, high):
     """
-        return smallest number among args bounded by low & high
+        Return smallest number among args bounded by low & high.
     """
-
+    res = float('inf')
+    for arg in args:
+        if arg < res and low < arg < high:
+            res = arg
+    return res
 
 def make_min(*, low, high):
     """
-        return inner function object which takes at last one argument
+        Return inner function object which takes at last one argument
         and return smallest number amount it's arguments, but if the
         smallest number is lower than the 'low' which given as required
         argument the inner function has to return it.
     """
-
     def inner(first, *args):
-        pass
+        result = float('inf')
+
+        for arg in (first,) + args:
+            if arg < result and low < arg < high:
+                result = arg
+        return result
 
     return inner
